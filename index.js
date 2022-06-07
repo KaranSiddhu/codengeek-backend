e6b8df77835f8b560bf5e8096eff8f79b796968a
karan siddhu <karansiddhu22@gmail.com>
2022-06-07 10:11:45 +0530

    Changed cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://codengeek.netlify.app/",
+      "https://codengeek.netlify.app",
       "http://localhost:3000"
     ],
     credentials: true
